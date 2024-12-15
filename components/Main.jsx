045c5e9fc0e336c693ef8a1f4d1b94d9f42ff7c6
excuse yuvi <yuvrajspannu@gmail.com>
2024-12-15 01:41:09 -0500

    Initial Commit (prob final one too)

---
+++ components/Main.jsx
@@ -0,0 +1,16 @@
+export default function Main() {
+    return (
+        <>
+        <h1 className="facts-header">Fun Facts About React</h1>
+        <ul className="facts-list">
+            <li>First released In 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Maintained by Meta</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+            <li>It is a library, not a frameworkmake</li>
+            <li>Instagram was one of the first platforms to adopt React during its early days</li>
+            </ul>
+        </>
+    )
+}
